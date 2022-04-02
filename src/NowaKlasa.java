5069ddeec3d59a36464f1e16708031e3d0963e5f
bKlamann92 <bartosz.klamann@gmail.com>
2022-04-02 12:24:32 +0200

    Drugi wpis w histoii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,22 @@
+/* komentarz wieloliniowy
+tutaj mogę towrzyć
+wiele
+linii kodu
+ */
+
+/**
+ * Ta klasa jest moją pierwszą klasą!
+ *
+ * Ten komentarz jest częścią dokumentacji!
+ */
+public class NowaKlasa {
+    // Do zrobienia
+    // TODO: musze wstawic pranie
+    // TODO: musze wstawic pranie
+    // TODO: musze wstawic pranie
+
+    // Kod powinien się sam dokumentować
+}
+
+
+
